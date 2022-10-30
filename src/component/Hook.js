d479e24220dc343cd61ac5074d5417594c485c23
fatimah06 <fatima.m.mutairi@gmail.com>
2022-10-30 19:30:51 +0300

    week3

--- src/component/Hook.js
+++ src/component/Hook.js
@@ -1,21 +1,42 @@
 import React from 'react'
-import { Button } from '@chakra-ui/react'
+import { Button, Input } from '@chakra-ui/react'
 // import React, {useState} from 'react'
 // اذا استعملت الطريقه هذي اللي مسويه لها كومنت لازم اعدل اللي تحت معها 
 
 function Hook() {
     const [counter, setCounter] = React.useState (0)
+    const [num , setNum] = React.useState(20)
+    const [todo, setTodo] = React.useState ('')
+    const [list, setList] = React.useState ([])
     // const [counter, setCounter] = useState (0)
     const increment = () => {
         console.log("set Counter");
         setCounter( (prev) => prev +1);
         // setCounter (counter + 1) ممكن  حل السطر اللي قبل بالشكل هذا 
     }
+    const descrement = () => {
+        setNum ((pre) => pre -1 );
+    }
+    const todoList = () => {
+        setList ([...list], todo)
+    }
   return (
     <div>
-        <Button onClick = {increment}>Click me</Button>
+        <Button onClick = {increment}>Increment</Button>
         <p>Counter : {counter}</p>
-    </div>
+        <p>__________________</p>
+        <Button onClick={descrement}> Descrement</Button>
+        <p>Descrement : {num}</p>
+        <Input onChange= {(e) => {setTodo(e.target.value)}}> </Input>
+        <Button onClick={todoList}>Add Task</Button>  
+        <p>{list.map(items =>
+            <ul>
+                <li>
+                    {items}
+                </li>
+            </ul>)}
+        </p> 
+         </div>
   )
 }
 
---
+++ src/component/calculator.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button, Input } from '@chakra-ui/react'
+
+
+function calculator() {
+    const [input1, setInput1] = React.useState('')
+    const [input2, setInput2] = React.useState('')
+  return (
+    <div>
+        <Button onClick={input1 + input2}>+</Button>
+    </div>
+  )
+}
+
+export default calculator
